60620321d3f4d53cc1bfb59a8c7d535e1144a05c
stephen <scheckley@gmail.com>
2024-02-24 17:57:10 +0000

    set neotree to open automatically

--- init.lua
+++ init.lua
@@ -727,3 +727,13 @@ vim.cmd [[
     autocmd FileType text setlocal linebreak
 ]]
 
+vim.api.nvim_create_autocmd("VimEnter", {
+  pattern = "*",
+  group = vim.api.nvim_create_augroup("NeotreeOnOpen", { clear = true }),
+  once = true,
+  callback = function(_)
+    if vim.fn.argc() == 0 then
+      vim.cmd("Neotree")
+    end
+  end,
+})
