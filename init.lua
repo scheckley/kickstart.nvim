3c48888bd67a60522c2f0412d56f7fc310aca084
Stephen Checkley <stephen.checkley@stfc.ac.uk>
2024-05-06 20:49:51 +0100

    removed yqx from config

--- init.lua
+++ init.lua
@@ -754,17 +754,6 @@ vim.api.nvim_create_autocmd("VimEnter", {
   end,
 })
 
-local jqx = vim.api.nvim_create_augroup("Jqx", {})
-vim.api.nvim_clear_autocmds({ group = jqx })
-vim.api.nvim_create_autocmd("BufWinEnter", {
-	pattern = { "*.json", "*.yaml" },
-	desc = "preview json and yaml files on open",
-	group = jqx,
-	callback = function()
-		vim.cmd.JqxList()
-	end,
-})
-
 vim.api.nvim_create_autocmd({ "FileType" }, {
   callback = function()
     if require("nvim-treesitter.parsers").has_parser() then
